2ff3df64e936ed032ab79ff592c99d96743b8802
Kazakovtsev Nikita <nikita_kazakovtsev@bk.ru>
2023-01-13 02:52:17 +0300

    asd

--- filtration.py
+++ filtration.py
@@ -31,15 +31,3 @@ r100 = result2.split('\n')
 print(r100)
 
 
-
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-#with open("phonebook.csv", "w") as f:
-#  datawriter = csv.writer(f, delimiter=',')
-#  Вместо contacts_list подставьте свой список
-#  datawriter.writerows(contacts_list)
-
-
